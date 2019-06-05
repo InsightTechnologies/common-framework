1bb9a0bd13b19f6822cdbe2d99a0faf7282c7e1c
samhithar <samhithaeega@gmail.com>
2019-06-05 09:45:06 -0500

    initial commit

---
+++ src/main/java/com/miracle/common/bean/FeatureWithEstimates.java
@@ -0,0 +1,73 @@
+package com.miracle.common.bean;
+
+public class FeatureWithEstimates {
+	private int featureID;
+	private int uid;
+	private String featureName;
+	private double effort;
+
+	/**
+	 * @return the featureID
+	 */
+	public int getFeatureID() {
+		return featureID;
+	}
+
+	/**
+	 * @param featureID the featureID to set
+	 */
+	public void setFeatureID(int featureID) {
+		this.featureID = featureID;
+	}
+
+	/**
+	 * @return the uid
+	 */
+	public int getUid() {
+		return uid;
+	}
+
+	/**
+	 * @param uid the uid to set
+	 */
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	/**
+	 * @return the featureName
+	 */
+	public String getFeatureName() {
+		return featureName;
+	}
+
+	/**
+	 * @param featureName the featureName to set
+	 */
+	public void setFeatureName(String featureName) {
+		this.featureName = featureName;
+	}
+
+	/**
+	 * @return the effort
+	 */
+	public double getEffort() {
+		return effort;
+	}
+
+	/**
+	 * @param effort the effort to set
+	 */
+	public void setEffort(double effort) {
+		this.effort = effort;
+	}
+
+	/* 
+	 */
+	@Override
+	public String toString() {
+		return "FeatureWithEstimates [featureID=" + featureID + ", uid=" + uid + ", featureName=" + featureName
+				+ ", effort=" + effort + "]";
+	}
+
+}
---
+++ src/main/java/com/miracle/common/response/FeatureResponse.java
@@ -0,0 +1,34 @@
+package com.miracle.common.response;
+
+public class FeatureResponse {
+	private Object object;
+	private boolean isSuccess;
+
+	/**
+	 * @return the object
+	 */
+	public Object getObject() {
+		return object;
+	}
+
+	/**
+	 * @param object the object to set
+	 */
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	/**
+	 * @return the isSuccess
+	 */
+	public boolean isSuccess() {
+		return isSuccess;
+	}
+
+	/**
+	 * @param isSuccess the isSuccess to set
+	 */
+	public void setSuccess(boolean isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+}
